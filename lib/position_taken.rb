0d17d10d33998659d83eff2787f100cdffad21b7
Kyle Harris <k_harris95@live.com>
2018-01-11 17:53:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if board[index] != " " && board[index] != ""
     return true
   end
-  if board[index]  == " " || board[index] == "" || board[index] == nil
+  if (board[index]  == " ") || (board[index] == "") || (board[index] == nil)
     return false
   end
 end
